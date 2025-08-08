3dfd31cbede64f294aaccbdfbcf0e3208c68b3fa
Fernando Lima <fernando@umavida.com>
2025-08-08 09:35:13 -0300

    Ajustes

--- GrpcServiceServer/Services/GreeterService.cs
+++ GrpcServiceServer/Services/GreeterService.cs
@@ -14,10 +14,8 @@ namespace GrpcServiceServer.Services
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             if (request.Count > 0 && request.Count % 50000 == 0)
-            {
-                _logger.LogInformation($"Received {request.Count} requests: {DateTime.Now:HH: mm: ss} ");
-                _logger.LogInformation("Received request to say hello to {Name}", request.Name);
-            }
+                Console.WriteLine($"Received {request.Count} requests: {DateTime.Now:HH:mm:ss}");
+
             return Task.FromResult(new HelloReply
             {
                 Message = "Hello " + request.Name
